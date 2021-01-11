553e31a85e8bc0ac048ac1df2c2e008dc1109d63
Agnaldo Burgo Junior <agnaldoburgojr@gmail.com>
2021-01-11 10:10:19 -0300

    feat: add icon to bottom navigation bar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack'
 import { MainScreen, ContactsScreen, DetailsScreen } from '../screens'
+import Icon from 'react-native-vector-icons/Feather'
+import colors from '../styles/colors'
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator()
@@ -9,7 +11,7 @@ const Stack = createStackNavigator()
 const StackNavigator = () => {
   return (
     <Stack.Navigator >
-      <Stack.Screen name="main" component={MainScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="main" component={MainScreen} options={{ headerShown: false }} />
       <Stack.Screen name="details" component={DetailsScreen} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
@@ -17,11 +19,30 @@ const StackNavigator = () => {
 
 const TabNavigator = () =>  {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='stack' component={StackNavigator} />
-      <Tab.Screen name='contacts' component={ContactsScreen} />
+    <Tab.Navigator tabBarOptions={tabBarOptions}>
+      <Tab.Screen name='stack' component={StackNavigator} options={mainOptions} />
+      <Tab.Screen name='contacts' component={ContactsScreen} options={contactsOptions}/>
     </Tab.Navigator>
   );
+}
+
+const tabBarOptions = {
+  activeTintColor: colors.accent,
+  inactiveTintColor: colors.lightGrey,
+}
+
+const mainOptions={
+  tabBarLabel: 'Cadastro',
+  tabBarIcon: ({ color }: any) => (
+    <Icon name="file-plus" color={color} size={26} />
+  ),
+}
+
+const contactsOptions = { 
+  tabBarLabel: 'Listagem',
+  tabBarIcon: ({color} : any) => (
+    <Icon name="list" color={color} size={26} />
+  ),
 }
 
 export default TabNavigator
